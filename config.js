bfee1c39d48fe310934983a3667d476d9db7635f
cuppi <cuppi413@163.com>
2018-11-22 19:23:45 +0800

    增加vue-cli版本限定

--- config.js
+++ config.js
@@ -5,9 +5,10 @@
 const path = require('path');
 const user_config = require(path.resolve(__dirname, '../../', '.jbz.oss.config.js'));
 const default_config = {
-    replaceInterceptor: (path, from, to) => {
-        return to;
-    }
+  vueCliVersion: 2,
+  replaceInterceptor: (path, from, to) => {
+    return to;
+  }
 }
 
 const config = Object.assign({}, default_config, user_config);
